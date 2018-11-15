1a8018d9d4105ec67f7388c79d7be8ed97996b09
Zuguang Liu <liu2z2@mail.uc.edu>
2018-11-15 18:35:50 -0500

    added test stats

---
+++ tests/algorithms/find_city.py
@@ -0,0 +1,7 @@
+# Author      : Zuguang Liu
+# Date        : 2018-11-15 17:04:30
+# Python Ver  : 3.6
+# Description : 
+
+addr='700 Riddle Rd, Cincinnati, OH'
+print(addr.split(',')[1].strip())
---
+++ tests/stats/stats_lib.py
@@ -0,0 +1,11 @@
+# Author      : Zuguang Liu
+# Date        : 2018-11-15 16:38:39
+# Python Ver  : 3.6
+# Description : 
+
+import pandas
+import datetime
+
+df=pandas.read_csv('gas_OH.csv')
+df['date']=[datetime.datetime.strptime(x, '%Y-%m-%d') for x in df['date']]
+df['address']=[x.split(',')[1].strip() for x in df['address']]
