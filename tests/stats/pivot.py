4a5bc081bcf5e2d6367d443bcbcf4ac6d05a0122
Zuguang Liu <liu2z2@mail.uc.edu>
2018-11-16 11:01:33 -0500

    update on stats test

--- tests/stats/pivot.py
+++ tests/stats/pivot.py
@@ -8,5 +8,5 @@ import numpy as np
 
 df = pd.DataFrame({"A": ["foo", "foo", "foo", "foo", "foo","bar", "bar", "bar", "bar"],"B": ["one", "one", "one", "two", "two","one", "one", "two", "two"],"C": ["small", "large", "large", "small","small", "large", "small", "small","large"],"D": [1, 2, 2, 3, 3, 4, 5, 6, 7]})
 
-table = pd.pivot_table(df, columns=['C'], aggfunc=[max,min,np.mean])
+table = pd.pivot_table(df, columns=['A'], aggfunc=[max,min,np.mean])
 print(table)
--- tests/stats/stats_lib.py
+++ tests/stats/stats_lib.py
@@ -5,8 +5,11 @@
 
 import pandas
 import datetime
+import numpy
 
 df=pandas.read_csv('gas_OH.csv')
 df['date']=[datetime.datetime.strptime(x, '%Y-%m-%d') for x in df['date']]
-df['address']=[x.split(',')[1].strip() for x in df['address']]
-print(df.describe())
+df['city']=[x.split(',')[1].strip() for x in df['address']]
+df=df.iloc[0:3]
+table=pandas.pivot_table(df,index=['service'], aggfunc=[max])
+print(list(table['max']['address']))
